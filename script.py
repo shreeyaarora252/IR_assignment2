5ac30982a0786bb9d1ba59015aba92925b20975c
shreeyaarora252 <109575610+shreeyaarora252@users.noreply.github.com>
2024-09-26 19:05:23 +0530

    inverted index

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import os 
+
 def read_documents_from_folder(folder_path):
     documents = {}
     doc_id_map = {}  # Map to store document ID -> filename
@@ -14,6 +16,28 @@ def read_documents_from_folder(folder_path):
 
     print("Document ID -> Filename Map:")
     for doc_id, filename in doc_id_map.items():
-        print(f"{doc_id}: {filename}")  # Debugging: Display mapping
+        print(f"{doc_id}: {filename}")  # debugging: Display mapping
 
     return documents
+
+
+def build_index(documents):
+    inverted_index = defaultdict(list)
+
+    for doc_id, text in documents.items():
+        term_freqs = defaultdict(int)
+        terms = text.lower().split()
+
+        for term in terms:
+            term_freqs[term] += 1
+
+        for term, tf in term_freqs.items():
+            inverted_index[term].append((doc_id, tf))
+    
+    print("Inverted index created")  # debugging
+    return inverted_index
+
+# Test the function
+inverted_index = build_index(documents)
+print(inverted_index)  
+
